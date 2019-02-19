f6a7c532dd3fab9aa5f6f6e01122ab04c67352df
INYEONGKIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-02-19 21:50:54 +0900

    Create BOJ2420.java

---
+++ BOJ2420.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s[] = br.readLine().split(" ");
+        bw.write(Math.abs((Long.parseLong(s[0])-Long.parseLong(s[1])))+"");
+        bw.flush();
+        bw.close();
+    }
+}
