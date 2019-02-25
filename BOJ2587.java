c21330997fbb5649e81e792a3254b5f4a5416c66
INYEONGKIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-02-26 01:09:00 +0900

    Create BOJ2587.java

---
+++ BOJ2587.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a[] = new int[5];
+        int sum=0;
+        for(int i=0; i<5; i++){
+            a[i]=Integer.parseInt(br.readLine());
+            sum+=a[i];
+        }
+        Arrays.sort(a);
+        bw.write(sum/5+"\n"+a[2]);
+
+        bw.flush();
+        bw.close();
+    }
+}
