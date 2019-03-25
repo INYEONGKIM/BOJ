5bbd50847b4c5912b61c69570d093bcd38faf498
INYEONGKIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-03-25 23:19:23 +0900

    Create BOJ1927.java

---
+++ BOJ1927.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        int n=Integer.parseInt(br.readLine());
+        int x;
+        while(n-->0){
+            x=Integer.parseInt(br.readLine());
+            if(x==0){
+                if(q.isEmpty())
+                    sb.append("0\n");
+                else
+                    sb.append(q.poll()+"\n");
+            }else {
+                q.offer(x);
+            }
+        }
+        System.out.print(sb);
+    }
+}
