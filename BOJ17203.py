d94dffde66e41171ab7238419f564225557b8650
INYEONG KIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-05-23 20:11:45 +0900

    Update BOJ17203.py

--- BOJ17203.py
+++ BOJ17203.py
@@ -1,9 +1,20 @@
 l,n=map(int,input().split())
-a=[int(i) for i in input().split()];r=""
+a=[int(i) for i in input().split()];r="";b=[]
+for i in range(l-1):
+    b.append(abs(a[i]-a[i+1]))
 for _ in range(n):
     s=0
     x,y=map(int,input().split())
-    for i in range(x,y):
-        s+=abs(a[i]-a[i-1])
+    s=sum(b[x-1:y-1])
     r+=str(s)+"\n"
 print(r,end="")
+
+# l,n=map(int,input().split())
+# a=[int(i) for i in input().split()];r=""
+# for _ in range(n):
+#     s=0
+#     x,y=map(int,input().split())
+#     for i in range(x,y):
+#         s+=abs(a[i]-a[i-1])
+#     r+=str(s)+"\n"
+# print(r,end="")
