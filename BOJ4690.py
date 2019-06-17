23be850b5ac1b7ce3d1f5f9baa09f64195572990
INYEONG KIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-06-17 18:54:57 +0900

    Create BOJ4690.py

---
+++ BOJ4690.py
@@ -0,0 +1,6 @@
+for a in range(6,101):
+    for b in range(2,a):
+        for c in range(b+1,a):
+            for d in range(c+1,a):
+                if a**3 == b**3+c**3+d**3:
+                    print("Cube = ",a,", Triple = (",b,",",c,",",d,")",sep="")
