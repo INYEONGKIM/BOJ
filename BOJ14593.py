8674608d3e3b8a47e4ba4ae5be5bcc62d52676b2
INYEONG KIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-09-12 01:11:24 +0900

    Create BOJ14593.py

---
+++ BOJ14593.py
@@ -0,0 +1,9 @@
+class f:
+    def __init__(self,i,s,c,d):
+        self.i=i;self.s=s;self.c=c;self.d=d
+l=[];input=__import__('sys').stdin.readline
+for i in range(int(input())):
+    s,c,d=map(int,input().split())
+    l.append(f(i,s,c,d))
+l.sort(key=lambda x:(-x.s,x.c,x.d))
+print(l[0].i+1)
