7cf63cf57611d2b041657f18acbe9abc2135e665
INYEONG KIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-09-25 17:45:51 +0900

    Update BOJ11048.py

--- BOJ11048.py
+++ BOJ11048.py
@@ -5,5 +5,5 @@ for i in range(n):
         if i==0 and j==0: continue
         elif i==0: d[i][j]+=d[i][j-1]
         elif j==0: d[i][j]+=d[i-1][j]
-        elif i>0 and j>0: d[i][j]+=max(d[i-1][j-1],d[i][j-1],d[i-1][j])
+        else: d[i][j]+=max(d[i-1][j-1],d[i][j-1],d[i-1][j])
 print(d[n-1][m-1])
