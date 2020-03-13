901a1e963f641477393e68bab32d212ab4484a8b
INYEONG KIM <42140395+INYEONGKIM@users.noreply.github.com>
2020-03-13 17:03:20 +0900

    Update BOJ2468.py

--- BOJ2468.py
+++ BOJ2468.py
@@ -1,4 +1,4 @@
-from collections import deque
+__import__('sys').setrecursionlimit(10**9)
 
 n=int(__import__('sys').stdin.readline())
 a = [[int(i) for i in input().split()] for _ in range(n)]
@@ -7,25 +7,25 @@ max_height = -1
 for x in a:
     max_height = max(max(x), max_height)
 
+
+def dfs(i, j):
+    visited[i][j] = True
+    for (x, y) in (i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1):
+        if not (0 <= x < n and 0 <= y < n):
+            continue
+        else:
+            if not visited[x][y]:
+                dfs(x, y)
+
 max_island = -1
 for height in range(max_height):
     visited = [[False if a[i][j] > height else True for j in range(n)] for i in range(n)]
     res = 0
-    q = deque()
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
                 visited[i][j] = True
                 res += 1
-                q.append((i,j))
-                while q:
-                    x, y = q.pop()
-                    visited[x][y] = True
-                    for (_x, _y) in (x-1, y), (x, y+1), (x+1, y), (x, y-1):
-                        if not (0 <= _x < n and 0 <= _y < n):
-                            continue
-                        else:
-                            if not visited[_x][_y]:
-                                q.append((_x, _y))
+                dfs(i, j)
     max_island = max(res, max_island)
 print(max_island)
